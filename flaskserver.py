2eec722c3277f55cc64a5c74d0b041b667097001
zhiyanggg <31137440+zhiyanggg@users.noreply.github.com>
2018-08-27 23:14:03 +0800

    Add files via upload
    Change the sentence patterns in loop1.
    
    Flask server implemented fb response. Mysql managed to login

--- flaskserver.py
+++ flaskserver.py
@@ -1,73 +1,116 @@
-from flask import Flask, request
+import urllib
+import json
+import os
 
+from flask import Flask
+from flask import request
+from flask import make_response
+from complexquery import *
+from sqlalchemy import *
+from sqlalchemy import create_engine
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
+import pprint
+import mysql
+from loop1 import food
+# Flask app should start in global layout
 app = Flask(__name__)
 
-FB_API_URL = 'https://graph.facebook.com/v2.6/me/messages'
-VERIFY_TOKEN = 'nihao'
-PAGE_ACCESS_TOKEN = "EAACj6UD3ZBiMBAAkKkhcd2vXz7uHRrIiat2jhbK8Xm0GyHTu39zSJJNWL6apVxGZCYJxpZBjP5D8Kl2ATtTepRBCUAyFie1f4f0uPZBaNujV6TvH8htzzMF6FpjXLo26Jgq8vl0ZCi17H6yTYETyKqiyQ7Cj577woGxWLvdwYRo76darUo2AluroICzlJDTgZD"
+
+@app.route('/', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeWebhookResult(req):
+    intentofDF = req.get("queryResult").get("intent").get("displayName")
+    if intentofDF == "Complexquery":
+        speech = complexq()
+        result = {
+            "fulfillmentText": speech,
+            # "source": "facebook",
+                "fulfillmentMessages": [
+                    # {
+                    #     "platform": "FACEBOOK",
+                    #     "card": {
+                    #         "title": "Title: this is a title",
+                    #         "subtitle": "This is an subtitle.  Text can include unicode characters including emoji 📱.",
+                    #         "imageUri": "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
+                    #         "buttons": [
+                    #             {
+                    #                 "text": "This is a button",
+                    #                 "postback": "https://assistant.google.com/"
+                    #             }
+                    #         ]
+                    #     }
+                    # },
+                    {
+                        "platform": "FACEBOOK",
+                        "text": {
+                            "text": [
+                                speech
+                            ]
+                        }
+                    }
+                ],
+            }
+        # print("type: ", type(json.dumps(result)))
+        return result
+
+    if intentofDF == "Complexquery - yes":
+        __DEBUG__ = True
+        # engine = create_engine('mysql+mysqlconnector://zy:zy123456@localhost/canteena_fnb?charset=utf8',
+        #                        encoding='utf8')
+        engine = create_engine('mysql+mysqlconnector://zy:zy123456@localhost/canteena_fnb', pool_recycle=3600)
+        print("logon to mysql")
+        Session = sessionmaker(bind=engine)
+        s = Session()
+        print("session created")
+        retStr = "Total price: S$"
+        totalprice = 0.0
+
+        for idx, item in enumerate(food):
+            query = s.query(Food.fprice).filter(Food.fname.in_([foods[0]]))
+            result = query.first()
+            unit_price = 3.0
+            if result:
+                unit_price = float(result[0])
+                if (__DEBUG__):
+                    print("Checking price of your order")
+                    pprint.pprint(result[0])
+
+            noItems = 1.0
+            if (idx < len(units)):
+                numberStr = ""
+                if (units[idx] != None):
+                    numberStr = str(units[idx]).replace("\"", "")
+                noItems = float(numberStr)
+            totalprice = totalprice + unit_price * noItems
+
+        if (__DEBUG__):
+            print("Total price: " + str(totalprice))
+
+        result1 = retStr + str(totalprice)
+        return result1
 
 
-def get_bot_response(message):
-    """This is just a dummy function, returning a variation of what
-    the user said. Replace this function with one connected to chatbot."""
-    return "This is a dummy response to '{}'".format(message)
 
 
-def verify_webhook(req):
-    if req.args.get("hub.verify_token") == VERIFY_TOKEN:
-        return req.args.get("hub.challenge")
-    else:
-        return "incorrect"
 
-def respond(sender, message):
-    """Formulate a response to the user and
-    pass it on to a function that sends it."""
-    response = get_bot_response(message)
-    send_message(sender, response)
-
-
-def is_user_message(message):
-    """Check if the message is a message from the user"""
-    return (message.get('message') and
-            message['message'].get('text') and
-            not message['message'].get("is_echo"))
-
-
-@app.route("/",methods=['GET','POST'])
-def listen():
-    # GET: validate facebook token
-	if request.method == 'GET':
-		valid = messenger.verify_webhook(request)
-		if not valid:
-			abort(400, 'Invalid Facebook Verify Token')
-		return valid
-
-    # POST: process message
-	output = request.get_json()
-	if 'entry' not in output:
-		return 'OK'
-
-	for entry in output['entry']:
-		if 'messaging' not in entry:
-			continue
-
-		for item in entry['messaging']:
-
-			# delivery notification (skip)
-			if 'delivery' in item:
-				continue
-
-			# get user
-			user = item['sender'] if 'sender' in item else None
-			if not user:
-				continue
-			else:
-				user_id = user['id']
-
-			# handle event
-			messenger.handle(user_id, item)
-
-	return "ok"
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0')
+    port = int(os.getenv('PORT', 5000))
+
+    print ("Starting app on port %d" %(port))
+
+    app.run(debug=True, port=port, host='0.0.0.0')
--- loop1.py
+++ loop1.py
@@ -1,11 +1,14 @@
-chinesefood = ("char kway teow", "biryani", "oyster omelette", "chicken rice", "pasta", "satay", "hokkien prawn mee", "fried carrot cake",
+food = ("char kway teow", "biryani", "oyster omelette", "chicken rice", "pasta", "satay", "hokkien prawn mee", "fried carrot cake",
                "hokkien char mee", "curry fish head", "noodle", "spaghetti with sausage", "beef", "rojak", "aglio olio chicken cutlet",
                "roti prata", "spaghetti with chicken chop", "aglio olio chicken chop", "bee hoon", "spaghetti with chicken cutlet",
                "bun bo hue", "spaghetti with ham and mushroom", "pho", "mee siam", "dim sum", "wanton mee", "pho bo", "laksa", "chicken burger",
                "bak chor mee", "crabs", "nasi lemak", "fish burger", "bak kut teh", "fried hokkien mee", "fish n chips")
 
-drinks = ("milk tea", "lao hor", "soy bean", "coffee", "kopi", "beer gao")
+drinks = ("milk tea", "lao hor", "soy bean", "coffee", "kopi", "beer gao", "milo", "tea", "teh", "teh bing", "milo bing",
+          "iced milo", "iced coffee", "iced milk tea")
+
 sides = ("fries", "mash potato", "toast cube", "salad", "soup", "spaghetti")
+
 numbers = open("C:/Users/Asus-Laptop/Desktop/numbers without word forms.txt", "r")
 numbers = numbers.read()
 
@@ -31,59 +34,76 @@ def numbertraining():
 
 
 def fooddatatraining():
-    for i in chinesefood:
+    for i in food:
         x = i
         TRAIN_DATA = (
-            ("1 large " + x + " please", {
-                'entities': [(0, 1, 'QUANTITY'),
-                             (2, 7, 'SIZE'),
-                             (8, 8+len(x), 'FOOD')]
-            }),
-
-            ("2 char kway teow 4 large biryani 5 large chicken rice", {
-                'entities': [(0, 1, 'QUANTITY'),
-                             (2, 16, 'FOOD'),
-                             (9, 18, 'QUANTITY'),
-                             (19, 24, 'SIZE'),
-                             (25, 32, 'FOOD'),
-                             (33, 34, 'QUANTITY'),
-                             (35, 40, 'SIZE'),
-                             (41, 53, 'FOOD')]
-            }),
 
             (x + " is a cuisine", {
                 'entities': [(0, len(x), 'FOOD')]
             }),
 
-            ("1 " + x + " 3 soup 4 biryani 5 kopi", {
-                'entities': [(0, 1, 'QUANTITY'),
-                             (2, 2+len(x), 'FOOD'),
-                             ( len(x) + 3,  len(x) + 4, 'QUANTITY'),
-                             ( len(x) + 5,  len(x) + 9, 'SIDES'),
-                             ( len(x) + 10,  len(x) + 11, 'QUANTITY'),
-                             ( len(x) + 12,  len(x) + 19, 'FOOD'),
-                             ( len(x) + 20,  len(x) + 21, 'QUANTITY'),
-                             ( len(x) + 22,  len(x) + 26, 'DRINKS')]
-            }),
-
             ("2 " + x, {
                 'entities': [(0, 1, 'QUANTITY'),
-                             (2,  2 + len(x), 'FOOD')]
-            }),
-
-            ("3 medium " + x + " 5 small chicken rice", {
-                'entities': [(0, 1, 'QUANTITY'),
-                             ( 2,  8, 'SIZE'),
-                             ( 9, ( 9 + len(x)), 'FOOD'),
-                             ( len(x) + 11,  len(x) + 12, 'QUANTITY'),
-                             (( 9 + len(x)) + 4, ( 9 + len(x)) + 9, 'SIZE'),
-                             (( 9 + len(x)) + 10, ( 9 + len(x)) + 22, 'FOOD')]
+                             (2, 2 + len(x), 'FOOD')]
             }),
 
             ("4 large " + x, {
                 'entities': [(0, 1, 'QUANTITY'),
-                             ( 2,  7, 'SIZE'),
-                             ( 8,  8 + len(x), 'FOOD')]
+                             (2, 7, 'SIZE'),
+                             (8, 8 + len(x), 'FOOD')]
+            }),
+
+            ("3 " + x + " 3 bak kut teh", {
+                'entities': [(0, 1, 'QUANTITY'),
+                             (2, 2 + len(x), 'FOOD'),
+                             (len(x) + 3, len(x) + 4, 'QUANTITY'),
+                             (len(x) + 5, len(x) + 16, 'FOOD')]
+            }),
+
+            ("3 medium " + x + " 5 small chicken rice", {
+                'entities': [(0, 1, 'QUANTITY'),
+                             (2, 8, 'SIZE'),
+                             (9, 9 + len(x), 'FOOD'),
+                             (len(x) + 10, len(x) + 11, 'QUANTITY'),
+                             (len(x) + 12, len(x) + 17, 'SIZE'),
+                             (len(x) + 18, len(x) + 30, 'FOOD')]
+            }),
+
+            ("2 small " + x + " 4 large biryani 5 large chicken rice", {
+                'entities': [(0, 1, 'QUANTITY'),
+                             (2, 7, 'SIZE'),
+                             (8, 8 + len(x), 'FOOD'),
+                             (9 + len(x), 10 + len(x), 'QUANTITY'),
+                             (11 + len(x), 16 + len(x), 'SIZE'),
+                             (17 + len(x), 24 + len(x), 'FOOD'),
+                             (25 + len(x), 26 + len(x), 'QUANTITY'),
+                             (27 + len(x), 31 + len(x), 'SIZE'),
+                             (32 + len(x), 44 + len(x), 'FOOD')]
+            }),
+
+
+
+            ("1 " + x + " 3 soup 4 biryani 5 kopi", {
+                'entities': [(0, 1, 'QUANTITY'),
+                             (2, 2+len(x), 'FOOD'),
+                             (len(x) + 3,  len(x) + 4, 'QUANTITY'),
+                             (len(x) + 5,  len(x) + 9, 'SIDES'),
+                             (len(x) + 10,  len(x) + 11, 'QUANTITY'),
+                             (len(x) + 12,  len(x) + 19, 'FOOD'),
+                             (len(x) + 20,  len(x) + 21, 'QUANTITY'),
+                             (len(x) + 22,  len(x) + 26, 'DRINKS')]
+            }),
+
+            ("2 small " + x + " 4 large coffee 5 medium fries", {
+                'entities': [(0, 1, 'QUANTITY'),
+                             (2, 7, 'SIZE'),
+                             (8, 8+len(x), 'FOOD'),
+                             (9+len(x), 10+len(x), 'QUANTITY'),
+                             (11+len(x), 16+len(x), 'SIZE'),
+                             (17+len(x), 23+len(x), 'DRINKS'),
+                             (24+len(x), 25+len(x), 'QUANTITY'),
+                             (26+len(x), 32+len(x), 'SIZE'),
+                             (33+len(x), 38+len(x), 'SIDES')]
             }),
         )
         for y in TRAIN_DATA:
@@ -94,53 +114,69 @@ def drinksdatatraining():
     for i in drinks:
         x = i
         TRAIN_DATA = (
-            ("2 medium " + x + " please", {
-                'entities': [(0, 1, 'QUANTITY'),
-                             ( 2,  8, 'SIZE'),
-                             ( 9,  9 + len(x), 'DRINKS')]
-            }),
-
-            ("1 small milk tea 3 large coffee 5 medium soy bean", {
-                'entities': [(0, 1, 'QUANTITY'),
-                             ( 2, 7, 'SIZE'),
-                             ( 8,  16, 'DRINKS'),
-                             (17, 18, 'QUANTITY'),
-                             (19,  24, 'SIZE'),
-                             ( 25,  31, 'DRINKS'),
-                             ( 32,  33, 'QUANTITY'),
-                             ( 34,  40, 'SIZE'),
-                             ( 41,  49, 'DRINKS')]
-            }),
-
-            ("1 small " + x + " 6 large spaghetti chicken chop", {
-                'entities': [(0, 1, 'QUANTITY'),
-                             (2, 7, 'SIZE'),
-                             (8,  8 + len(x), 'DRINKS'),
-                             (8+len(x)+1, 8+len(x)+2, 'QUANTITY'),
-                             (8+len(x)+3, 8+len(x)+8, 'SIZE'),
-                             (8+len(x)+9, 8+len(x)+31, 'FOOD')]
-            }),
 
             (x + " taste good", {
                 'entities': [(0, len(x), 'DRINKS')]
             }),
 
-             ("1 " + x, {
+            ("1 " + x, {
                 'entities': [(0, 1, 'QUANTITY'),
-                             (2,  2 + len(x), 'DRINKS')]
+                             (2, 2 + len(x), 'DRINKS')]
+            }),
+
+            ("2 medium " + x + " please", {
+                'entities': [(0, 1, 'QUANTITY'),
+                             (2,  8, 'SIZE'),
+                             (9,  9 + len(x), 'DRINKS')]
+            }),
+
+            ("1 small " + x + " 6 large spaghetti chicken chop", {
+                'entities': [(0, 1, 'QUANTITY'),
+                             (2, 7, 'SIZE'),
+                             (8, 8 + len(x), 'DRINKS'),
+                             (9 + len(x), 10 + len(x), 'QUANTITY'),
+                             (11 + len(x), 16 + len(x), 'SIZE'),
+                             (17 + len(x), 39 + len(x), 'FOOD')]
+            }),
+
+            ("1 small " + x + " 3 large coffee 5 medium soy bean", {
+                'entities': [(0, 1, 'QUANTITY'),
+                             (2, 7, 'SIZE'),
+                             (8,  8 + len(x), 'DRINKS'),
+                             (9 + len(x), 10 + len(x), 'QUANTITY'),
+                             (11 + len(x),  16 + len(x), 'SIZE'),
+                             (17 + len(x),  23 + len(x), 'DRINKS'),
+                             (24 + len(x),  25 + len(x), 'QUANTITY'),
+                             (26 + len(x),  32 + len(x), 'SIZE'),
+                             (33 + len(x),  41 + len(x), 'DRINKS')]
+            }),
+
+            ("3 medium " + x + " 5 small milo", {
+                'entities': [(0, 1, 'QUANTITY'),
+                             (2, 8, 'SIZE'),
+                             (9, 9 + len(x), 'DRINKS'),
+                             (len(x) + 10, len(x) + 11, 'QUANTITY'),
+                             (len(x) + 12, len(x) + 17, 'SIZE'),
+                             (len(x) + 18, len(x) + 22, 'DRINKS')]
             }),
 
             ("3 " + x + "  3 beer gao", {
                 'entities': [(0, 1, 'QUANTITY'),
-                             (2, ( 2 + len(x)), 'DRINKS'),
-                             ( len(x) + 3,  len(x) + 3 + 1, 'QUANTITY'),
-                             ( len(x) + 4 + 1,  len(x) + 12 + 1, 'DRINKS')]
+                             (2, 2 + len(x), 'DRINKS'),
+                             (len(x) + 3,  len(x) + 4, 'QUANTITY'),
+                             (len(x) + 5,  len(x) + 13, 'DRINKS')]
             }),
 
-             ("3 large " + x, {
+            ("4 small " + x + " 5 small mash potato 2 medium nasi lemak ", {
                 'entities': [(0, 1, 'QUANTITY'),
-                             ( 2,  7, 'SIZE'),
-                             ( 8,  8 + len(x), 'DRINKS')]
+                             (2, 7, 'SIZE'),
+                             (8,  8 + len(x), 'DRINKS'),
+                             (9 + len(x),  10 + len(x), 'QUANTITY'),
+                             (11 + len(x), 16 + len(x), 'SIZE'),
+                             (17 + len(x), 28 + len(x), 'SIDES'),
+                             (29 + len(x), 30 + len(x), 'QUANTITY'),
+                             (31 + len(x), 37 + len(x), 'SIZE'),
+                             (38 + len(x), 48 + len(x), 'FOOD')]
             }),
         )
         for y in TRAIN_DATA:
@@ -151,6 +187,16 @@ def sidesdatatraining():
     for i in sides:
         x = i
         TRAIN_DATA = (
+
+            (x + " taste good", {
+                'entities': [(0, len(x), 'SIDES')]
+            }),
+
+            ("3 " + x, {
+                'entities': [(0, 1, 'QUANTITY'),
+                             (2, 2 + len(x), 'SIDES')]
+            }),
+
             ("1 small " + x + " please", {
                 'entities': [(0, 1, 'QUANTITY'),
                              (2, 7, 'SIZE'),
@@ -171,33 +217,48 @@ def sidesdatatraining():
 
              ("2 large " + x, {
                 'entities': [(0, 1, 'QUANTITY'),
-                             ( 2,  7, 'SIZE'),
-                             ( 8,  8 + len(x), 'SIDES')]
+                             (2,  7, 'SIZE'),
+                             (8,  8 + len(x), 'SIDES')]
             }),
 
-            (x + " taste good", {
-                'entities': [(0, len(x), 'SIDES')]
-            }),
-
-             ("3 " + x, {
+            ("8 " + x + "  9 salad", {
                 'entities': [(0, 1, 'QUANTITY'),
-                             (2,  2 + len(x), 'SIDES')]
+                             (2, 2 + len(x), 'SIDES'),
+                             (len(x) + 3, len(x) + 4, 'QUANTITY'),
+                             (len(x) + 5, len(x) + 10, 'SIDES')]
             }),
 
-            ("3 " + x + " 5 toast cube 9 lao hor", {
+            ("3 small " + x + " 5 medium toast cube 9 large lao hor", {
                 'entities': [(0, 1, 'QUANTITY'),
-                             ( 2,  2 + len(x), 'SIDES'),
-                             ( 3 + len(x),  4 + len(x), 'QUANTITY'),
-                             ( 5 + len(x),  15 + len(x), 'SIDES'),
-                             ( 16 + len(x),  17 + len(x), 'QUANTITY'),
-                             ( 18 + len(x),  25 + len(x), 'DRINKS')]
+                             (2, 7, 'SIZE'),
+                             (8,  8 + len(x), 'SIDES'),
+                             (9 + len(x),  10 + len(x), 'QUANTITY'),
+                             (11 + len(x), 12 + len(x), 'SIZE'),
+                             (13 + len(x),  23 + len(x), 'SIDES'),
+                             (24 + len(x),  25 + len(x), 'QUANTITY'),
+                             (26 + len(x), 27 + len(x), 'SIZE'),
+                             (28 + len(x),  35 + len(x), 'DRINKS')]
             }),
 
-            ("2 " + x + " 7 toast cube", {
+            ("2 large " + x + " 7 large toast cube", {
                 'entities': [(0, 1, 'QUANTITY'),
-                             ( 2, ( 2 + len(x)), 'SIDES'),
-                             ( len(x) + 3,  len(x) + 3 + 1, 'QUANTITY'),
-                             ( len(x) + 4 + 1,  len(x) + 14 + 1, 'SIDES')]
+                             (2, 7, 'SIZE'),
+                             (8, 8 + len(x), 'SIDES'),
+                             (len(x) + 9,  len(x) + 10, 'QUANTITY'),
+                             (len(x) + 11, len(x) + 16, 'SIZE'),
+                             (len(x) + 17,  len(x) + 27, 'SIDES')]
+            }),
+
+            ("5 large " + x + " 2 large bak chor mee 4 medium kopi", {
+                'entities': [(0, 1, 'QUANTITY'),
+                             (2, 7, 'SIZE'),
+                             (8, 8 + len(x), 'SIDES'),
+                             (9 + len(x), 10 + len(x), 'QUANTITY'),
+                             (11 + len(x), 16 + len(x), 'SIZE'),
+                             (17 + len(x), 29 + len(x), 'FOOD'),
+                             (30 + len(x), 31 + len(x), 'QUANTITY'),
+                             (32 + len(x), 38 + len(x), 'SIZE'),
+                             (39 + len(x), 43 + len(x), 'DRINKS')]
             }),
         )
         for y in TRAIN_DATA:
